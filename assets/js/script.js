6182cc19670f9757da9a2a2f1fceeddb637df754
Soumeya <soumeyahassan47@gmail.com>
2021-03-16 12:26:48 +0000

    commented plan

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,31 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// lowercaseArray[]
+
+// prompt user for length of password store as variable
+
+// convert string to number parseInt()
+
+// validated length of password
+
+// isLowercase = confirm(do you want) etc
+
+// declare optionsArray
+
+// if (isLowercase) optionsArray.concat()
+
+// shuffle array
+
+// get random optionsArray character using index
+
+//store in randomArray
+
+//change randomArray to string
+
+//set up main function
